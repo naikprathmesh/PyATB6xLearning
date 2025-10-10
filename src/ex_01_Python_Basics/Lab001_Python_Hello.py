f8e46a2dfc447c11d10e92433b42f9b95017e277
Prathamesh Naik <nkprathmesh@gmail.com>
2025-10-11 01:59:37 +0530

    11-OCT-2025

--- src/ex_01_Python_Basics/Lab001_Python_Hello.py
+++ src/ex_01_Python_Basics/Lab001_Python_Hello.py
@@ -1,2 +1,1 @@
 print("Welcome to Python!")
-print("Pramod",123,"Amit","John",sep="*",end="_")
---
+++ src/ex_01_Python_Basics/Lab003_print.py
@@ -0,0 +1,3 @@
+print("Prathamesh",30,"Automation","Python",sep='_',end='\t')
+print("Prathamesh",30,"Automation","Selenium",sep='*',end='\t')
+print("Prathamesh",30,"Automation","API",sep='--',end='\t')
